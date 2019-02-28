7c43c8260b10e030bea9a8b327ba26659a25c8d2
metas1 <metas1@storage1.mats6.com>
2019-02-28 15:39:34 +0100

    test - User Input

--- js/main.js
+++ js/main.js
@@ -2,53 +2,66 @@
 var GameState = {
   //load the game assets before the game starts
   preload: function() {
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('pic1', 'assets/images/pic1.png');
-    this.load.image('pic2', 'assets/images/pic2.png');
-    this.load.image('pic3', 'assets/images/pic3.png');
-    this.load.image('pic4', 'assets/images/pic4.png');
+    this.game.load.image('background', 'assets/images/background.png');
+    this.game.load.image('arrow', 'assets/images/arrow.png');
+    this.game.load.image('pic1', 'assets/images/pic1.png');
+    this.game.load.image('pic2', 'assets/images/pic2.png');
+    this.game.load.image('pic3', 'assets/images/pic3.png');
+    this.game.load.image('pic4', 'assets/images/pic4.png');
     
   },
   //executed after everything is loaded
   create: function() {
-
+    
+    //scaling options
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
+    
+    //have the game centered horizontally
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
-  
+    
     //create a sprite for the background
-    this.background = this.game.add.sprite(0, 0, 'background');
+    this.background = this.game.add.sprite(0, 0, 'background')
     
-    //center of the world
-    this.pic1 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pic1');
-    
-    //place a sprite by it's center, not the top-left corner
-    this.pic1.anchor.setTo(0.5, 0.5); // or just this.chicken.anchor.setTo(0.5)
-
-    this.pic1.scale.setTo(2, 1);
-
-    this.pic2 = this.game.add.sprite(120, 10, 'pic2');
-    this.pic2.scale.setTo(0.5);
-	
-    this.pic3 = this.game.add.sprite(500, 300, 'pic3');
+    this.pic3 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pic3');
     this.pic3.anchor.setTo(0.5);
 
-	//flip on x
-    this.pic3.scale.setTo(-1, 1);	
+    //enable user input on sprite
+    this.pic3.inputEnabled = true;
+    this.pic3.input.pixelPerfectClick = true;
+    this.pic3.events.onInputDown.add(this.animateAnimal, this);
 
-    this.pic4 = this.game.add.sprite(100, 250, 'pic4');
-    this.pic4.scale.setTo(0.5);
-    this.pic4.anchor.setTo(0.5);
+    //left arrow
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
 
-    //rotate 90 degrees clockwise
-    this.pic4.angle = 90;
-	
+    //left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    //right arrow
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    //right arrow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);    
+
   },
   //this is executed multiple times per second
   update: function() {
-    this.pic4.angle += 0.5;	  
   },
+  switchAnimal: function(sprite, event) {
+    console.log('move animal');
+  },
+  animateAnimal: function(sprite, event) {
+    console.log('animate animal');
+  }
   
 
 };
